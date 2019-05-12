98e9c46686fe4439549f11213c8cfac698b5b244
Javier Moran <jmoranm1987@gmail.com>
2019-05-12 09:54:32 -0500

    Added .env.example, updated docs url and updated packages

--- app/app.js
+++ app/app.js
@@ -18,6 +18,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({ limit: '50mb'}));
 app.use(cors);
 
+const swaggerDocument = require('./docs/swagger.json');
 const base = '/api';
 app.use(`${base}/version`, routes.version);
 app.use(`${base}/users`, routes.users);
@@ -25,9 +26,11 @@ app.use(`${base}/collections`, routes.collections);
 app.use(`${base}/types`, routes.types);
 app.use(`${base}/items`, routes.items);
 app.use(`${base}/statistics`, routes.statistics);
+app.use(`${base}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const swaggerDocument = require('./docs/swagger.json');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', (req, res) => {
+  res.redirect(`${base}/docs`);
+});
 
 app.get('*', function(req, res, next) {
   res.sendFile(__dirname + '/public/index.html');
